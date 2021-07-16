71304b6c30e2c21930fee410d0c64973f937a2c0
mboinardakani <mobinardakani@hotmail.com>
2021-07-16 14:21:43 +0430

    everything in First commit :)

---
+++ mysite/blog/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Post, Comment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'publish')
+    list_filter = ('status', 'publish', 'create')
+    search_fields = ('title', 'body')
+    ordering = ('status', 'publish')
+    date_hierarchy = 'publish'
+    prepopulated_fields = {'slug': ('title', )}
+    raw_id_fields = ('author', )
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
---
+++ mysite/blog/models.py
@@ -0,0 +1,55 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+from taggit.managers import TaggableManager
+
+
+class PublishManger(models.Manager):
+    def get_queryset(self):
+        return super(PublishManger, self).get_queryset().filter(status='published')
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (('draft', 'Draft'), ('published', 'Published'))
+    title = models.CharField(max_length=250)
+    slug = models.CharField(max_length=250, unique_for_date='publish')
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    publish = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    tags = TaggableManager()
+
+    objects = models.Manager()
+    published = PublishManger()
+
+    class Meta:
+        ordering = ('-publish', )
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
+
